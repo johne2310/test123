cfbbeeb26391ef6116bd10064c421654e715b8ce
John Evans <john@day41.com.au>
2020-02-01 13:31:37 +1100

    Update firebase.js
    Remove firebase credentials

--- Vue/ninja/ninja-smoothies/src/firebase.js
+++ Vue/ninja/ninja-smoothies/src/firebase.js
@@ -2,13 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const app = firebase.initializeApp({
-  apiKey: 'AIzaSyB02qX2PjUurCQJMy-0vBk2Hedtn55Wpx0',
-  authDomain: 'fir-crud-410e8.firebaseapp.com',
-  databaseURL: 'https://fir-crud-410e8.firebaseio.com',
-  projectId: 'fir-crud-410e8',
-  storageBucket: 'fir-crud-410e8.appspot.com',
-  messagingSenderId: '514406660411',
-  appId: '1:514406660411:web:e62641e9412c3efb625f1e',
+ 
 });
 
 const db = app.firestore();
