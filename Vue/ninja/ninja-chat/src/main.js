94819787985ba305ca3d6d39da12ceb6e9edac98
johne2310 <john@day41.com.au>
2020-02-01 13:51:32 +1100

    Updated main.js

--- Vue/ninja/ninja-chat/src/main.js
+++ Vue/ninja/ninja-chat/src/main.js
@@ -1,12 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import vuetify from './plugins/vuetify';
+
+import './firebase';
+import { firestorePlugin } from 'vuefire';
+
+Vue.use(firestorePlugin);
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
-}).$mount("#app");
+  render: h => h(App),
+}).$mount('#app');
